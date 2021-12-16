3db68d7cfcb50399c92f12bb39f4dce1d2e8950e
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-12-16 15:22:15 -0700

    formated and styled sites, added images

--- models/product.js
+++ models/product.js
@@ -28,12 +28,16 @@ const productSchema = new mongoose.Schema({
 		type: String,
 		required: [true, "Must Provide Company"],
 		enum: {
-			values: ['ikea', 'puma', 'adidas', 'yeezy', 'nike', 'terraria'],
+			values: ['ikea', 'puma', 'adidas', 'yeezy', 'Nike', 'terraria', 'Foot Cuisine', 'Wacky N Tacky', 'All Nature Al', 'Plain Ol Corp.', "Electrono's"],
 			message: '{VALUE} is not supported'
 		},
 		trim: true,
 		maxlength: [50, 'Name Cannot Be more than 50 Characters'],
 	},
+	image: {
+		type: String,
+		require: [true, "Must Provide Image"]
+	}
 })
 
 module.exports = mongoose.model('Product', productSchema)
--- public/browser-app.js
+++ public/browser-app.js
@@ -1,8 +1,16 @@
-const display = document.querySelector('.tasks-container');
+const display = document.querySelector('#productWrap');
 
 const showProducts = async () => {
   const { data: products } = await axios.get('/store/v1/products')
-  console.log(products)
-  display.innerHTML = products.map((product) => { return product.name });
+  display.innerHTML = products.map(product => {
+    return `<div class="product">
+      <h1>${product.name}</h1>
+      <h2>${product.rating}</h2>
+      <img src="../images/${product.image}"/>
+      <h3>${product.company}</h3>
+      <h3>${new Intl.DateTimeFormat('en-US').format(new Date(product.createdAt))}</h3>
+      <h6>${product.price}</h6>
+      <h3>${product.featured}</h3>
+    </div>` }).reduce((a, c) => a + c);
 }
 showProducts()
